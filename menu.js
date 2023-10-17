e112db662f2dec1398770acd87a5a68b001f7f85
https://github.com/ThatianaMattos <thatianamattos25@gmail.com>
2023-10-17 20:26:17 -0300

    mais ajustes

--- menu.js
+++ menu.js
@@ -14,3 +14,14 @@ menu.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu')
 })
+function openModal(){
+    const modal = document.getElementById('modal-container')
+    modal.classList.add('mostrar')
+
+    modal.addEventListener('click', (e) =>{
+        if (e.target.id == 'modal-container' || e.target.id == "fechar"){
+            modal.classList.remove('mostrar')
+            localStorage.fechaModal = 'modal-container'
+        }
+    })
+}
